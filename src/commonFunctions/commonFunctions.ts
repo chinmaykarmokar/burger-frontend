bdcbf981ba2551159e882e5810726e7a569304a1
Chinmay Karmokar <chinmaykarmokar@gmail.com>
2023-01-12 01:58:36 +0530

    Integrated Menu API on customer login. Integrated Add To Cart API. Created common function for getting user specific cart items. Created actions and reducers for the same.

--- src/commonFunctions/commonFunctions.ts
+++ src/commonFunctions/commonFunctions.ts
@@ -11,11 +11,18 @@ import axios from "axios";
 
 // Import actions
 import { getCompleteMenu } from "../state/actions/customerActions";
-// const dispatch = useDispatch();
+import { getCartItems } from "../state/actions/customerActions";
 
 export const getMenuItems = async (dispatch: any, configParams: Object) => {
     await axios.get("http://localhost:3000/api/customers/menu", configParams)
     .then((response) => {
         dispatch(getCompleteMenu(response?.data?.data));
     }) 
+}
+
+export const getItemsFromCart = async (dispatch: any, configParams: Object) => {
+    await axios.get("http://localhost:3000/api/customers/getCartItems", configParams)
+    .then((response) => {
+        dispatch(getCartItems(response?.data?.data));
+    })
 }
--- src/components/GetCustomerComponent/getCustomerComponent.tsx
+++ src/components/GetCustomerComponent/getCustomerComponent.tsx
@@ -11,15 +11,19 @@ import axios from "axios";
 
 // Import actions
 import { getCustomer } from "../../state/actions/customerActions";
+import { addToCart } from "../../state/actions/customerActions"
 
 // Import common functions
-import { getMenuItems } from "../../commonFunctions/commonFunctions";
+import { getMenuItems, getItemsFromCart } from "../../commonFunctions/commonFunctions";
 
 const GetCustomerComponent: React.FC = () => {
     const dispatch = useDispatch();
 
+    const [config,setConfig] = useState<Object>();
+
     const customerData = useSelector((state: any) => {return state?.customers?.getCustomerData});
     const menuData = useSelector((state: any) => {return state?.customers?.completeMenuData});
+    const cartData = useSelector((state: any) => {return state?.customers?.completeCartData});
 
     const [hydrated, setHydrated] = useState(false);
 
@@ -55,6 +59,44 @@ const GetCustomerComponent: React.FC = () => {
         getMenuItems(dispatch, config);
     },[])
 
+    useEffect(() => {
+        const config = {
+            headers: {
+                "authorization": `Bearer ${localStorage.getItem("customer_token")}`
+            }
+        } 
+        
+        getItemsFromCart(dispatch, config);
+    },[])
+
+    const addBurgerToCart = async (configParams: Object, email: string, burgerName: string, burgerPrice: number, newBurgerPrice: number, burgerIDFromMenu: number) => {
+        const itemToBeAddedInCart = {
+            email: email,
+            burger_name: burgerName,
+            burger_price: burgerPrice,
+            new_burger_price: newBurgerPrice
+        }
+
+        await axios.post(`http://localhost:3000/api/customers/addToCart/${burgerIDFromMenu}`, itemToBeAddedInCart, configParams)
+        .then((response) => {
+            console.log(response)
+            dispatch(addToCart(response?.data));
+            alert(response?.data?.message);
+        })
+    }
+
+    useEffect(() => {
+        const config = {
+            headers: {
+                "authorization": `Bearer ${localStorage.getItem("customer_token")}`,
+                "content-type": "application/json",
+                "Access-Control-Allow-Origin": "*"
+            }
+        }
+
+        setConfig(config);
+    },[])
+
     console.log(customerData);
     console.log(menuData);
 
@@ -62,7 +104,27 @@ const GetCustomerComponent: React.FC = () => {
         <>
             {
                 (hydrated && typeof(window) !== "undefined" && localStorage.getItem("customer_token")) ?
-                    <h1>Welcome {customerData?.[0]?.firstname} {customerData?.[0]?.lastname}</h1>
+                    <>
+                        <h1>Welcome {customerData?.[0]?.firstname} {customerData?.[0]?.lastname}</h1>
+                        {
+                            menuData?.map((singleMenuItem: any) => {
+                                return (
+                                    <div>
+                                        <h4>{singleMenuItem?.burger_name} ({singleMenuItem?.category})</h4>
+                                        <h4>₹{singleMenuItem?.price}</h4>
+                                        <button
+                                            onClick = {() => {
+                                                addBurgerToCart(config, customerData?.[0]?.email, singleMenuItem?.burger_name, singleMenuItem?.price, singleMenuItem?.price, singleMenuItem?.id)
+                                            }}
+                                        >
+                                        {/* <button onClick={() => {addBurgerToCart(singleMenuItem?.id)}}> */}
+                                            Add To Cart
+                                        </button>
+                                    </div>
+                                )
+                            })
+                        }
+                    </>
                 :
                     <h1>You are not authorized.</h1>
             }
