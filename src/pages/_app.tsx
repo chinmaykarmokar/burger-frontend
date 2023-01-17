847a6035b3e996163806943571959e49b862a5c3
Chinmay Karmokar <chinmaykarmokar@gmail.com>
2023-01-18 02:47:35 +0530

    Installed react-bootstrap and completed the landing page. Modified a few CSS files.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,12 @@
-// import '../styles/globals.css'
+import "../../styles/globals.css";
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 
 // Import store
 import store from '../state/store/store';
+
+// Use React-Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
